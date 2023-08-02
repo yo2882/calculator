52a8e27ba9caeb0839e6c07be421cc748b3b201f
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-08-02 13:25:38 +0700

    Add boilerplate files and basic math/operate func

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let firstNum = 0
+let secondNum = 0
+let operator = null
+let result = 0
+
+function plus (){
+    return parseFloat(firstNum)+parseFloat(secondNum);
+}
+
+function minus (){
+    return parseFloat(firstNum)-parseFloat(secondNum);
+}
+
+function multiply (){
+    return parseFloat(firstNum)*parseFloat(secondNum);
+}
+
+function divide (){
+    return parseFloat(firstNum)/parseFloat(secondNum);
+}
+
+function operate(){
+    switch (operator) {
+        case "+":
+            return result = plus()
+            break;
+        case "-":
+            return result = minus()
+            break;    
+        case "x":
+            return result = multiply()
+            break;
+        case "÷":
+            return result = divide()
+            break;  
+    }
+}
+
